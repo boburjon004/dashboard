4925b784f95836ebcf99777701c37de090eeb269
boburjon004 <yaxyoxojayevbobur345@gmail.com>
2023-02-17 18:58:38 +0500

    Cool project

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customuser/', views.customuser),
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk_test>/', views.customer, name='customer'),
 ]  
